7d84d4cee2b29c4bf928eeedf8ba35727c9418ad
aischooloff-pixel <aischool.off@gmail.com>
2026-02-22 23:35:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = ""
+    TELEGRAM_BOT_TOKEN: str = "8446253574:AAG0nu6A5Ich-Vu65ryOgnudaqegEepaHB4"
     PROXY6_API_KEY: str = ""
     CRYPTOBOT_API_TOKEN: str = ""
     DATABASE_URL: str = "sqlite+aiosqlite:///bot.db"
