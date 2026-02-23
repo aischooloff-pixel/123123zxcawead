29209aa2f4de99e8aaeb20f4b83dd84bf218b97e
aischooloff-pixel <aischool.off@gmail.com>
2026-02-23 23:00:47 +0300

    Update TELEGRAM_BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
-import os
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = "8446253574:AAG0nu6A5Ich-Vu65ryOgnudaqegEepaHB4"
-    PROXY6_API_KEY: str = "4dbaf82e5e-35d6ccc64a-4ca7986c2e"
-    CRYPTOBOT_API_TOKEN: str = ""
-    DATABASE_URL: str = "sqlite+aiosqlite:///bot.db"
-
-    class Config:
-        env_file = ".env"
-        extra = "ignore"
-
-settings = Settings()
+TELEGRAM_BOT_TOKEN = '8596061120:AAFKtfaDD7P2tgXuWH2o-NsJgKKyhlBChWY'
